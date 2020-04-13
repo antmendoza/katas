736024a5e6a34d4130d708d2b940804967126c23
Antonio Mendoza Pérez <antmendoza@gmail.com>
2020-04-13 17:54:06 +0200

    cleanUp

--- kotlin/src/main/kotlin/com/antmendoza/katas/fibonacci/Fibonacci.kt
+++ kotlin/src/main/kotlin/com/antmendoza/katas/fibonacci/Fibonacci.kt
@@ -3,6 +3,10 @@ package com.antmendoza.katas.fibonacci
 class Fibonacci {
     fun inPosition(i: Int): Int {
 
+        if(i === 1 || i === 2){
+            return 1;
+        }
+
         return 0
     }
 
--- kotlin/src/test/kotlin/com/antmendoza/katas/fibonacci/FibonacciTest.kt
+++ kotlin/src/test/kotlin/com/antmendoza/katas/fibonacci/FibonacciTest.kt
@@ -15,5 +15,18 @@ class FibonacciTest {
 
 
 
+    @Test
+    fun `Should return 1 for position 1 `() {
+        assertEquals(1, Fibonacci().inPosition(1))
+    }
+
+
+    @Test
+    fun `Should return 1 for position 2 `() {
+        assertEquals(1, Fibonacci().inPosition(2))
+    }
+
+
+
 }
 
