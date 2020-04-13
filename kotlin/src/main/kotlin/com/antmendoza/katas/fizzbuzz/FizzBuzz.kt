5d5a1bc98c888418720ca35aedbfbd28a07c2964
Antonio Mendoza Pérez <antmendoza@gmail.com>
2020-04-13 17:39:39 +0200

    grenn

--- kotlin/src/main/kotlin/com/antmendoza/katas/fizzbuzz/FizzBuzz.kt
+++ kotlin/src/main/kotlin/com/antmendoza/katas/fizzbuzz/FizzBuzz.kt
@@ -3,7 +3,7 @@ package com.antmendoza.katas.fizzbuzz
 class FizzBuzz(var i: Int) {
     fun print(): String {
 
-        if (this.i === 5 || this.i === 10|| this.i === 15) {
+        if (this.i%5 === 0) {
             return "Buzz";
         }
 
